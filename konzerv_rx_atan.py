4239fff9935f738c8b77f298cb7a693f6d2c1dd2
László Mérő <laci37@gmail.com>
2021-03-27 13:32:10 +0100

    add atan demod, fix low pass freqs, name stuff

--- konzerv_rx_atan.py
+++ konzerv_rx_atan.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Konzerv Rx
-# Generated: Sat Mar 27 12:55:23 2021
+# Title: Konzerv Rx Atan
+# Generated: Sat Mar 27 12:54:08 2021
 ##################################################
 
 if __name__ == '__main__':
@@ -16,7 +16,6 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
-from gnuradio import analog
 from gnuradio import audio
 from gnuradio import blocks
 from gnuradio import eng_notation
@@ -33,10 +32,10 @@ from optparse import OptionParser
 import wx
 
 
-class konzerv_rx(grc_wxgui.top_block_gui):
+class konzerv_rx_atan(grc_wxgui.top_block_gui):
 
     def __init__(self):
-        grc_wxgui.top_block_gui.__init__(self, title="Konzerv Rx")
+        grc_wxgui.top_block_gui.__init__(self, title="Konzerv Rx Atan")
         _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
         self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
@@ -73,18 +72,18 @@ class konzerv_rx(grc_wxgui.top_block_gui):
         	proportion=1,
         )
         self.Add(_center_freq_sizer)
-        self.wxgui_waterfallsink2_1 = waterfallsink2.waterfall_sink_c(
+        self.wxgui_waterfallsink2_1 = waterfallsink2.waterfall_sink_f(
         	self.GetWin(),
         	baseband_freq=0,
         	dynamic_range=100,
         	ref_level=0,
         	ref_scale=2.0,
         	sample_rate=samp_rate,
-        	fft_size=512,
+        	fft_size=2048,
         	fft_rate=15,
         	average=False,
         	avg_alpha=None,
-        	title="Baseband signal",
+        	title="Demodulated signal",
         )
         self.Add(self.wxgui_waterfallsink2_1.win)
         self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_c(
@@ -101,24 +100,45 @@ class konzerv_rx(grc_wxgui.top_block_gui):
         	title="Received signal",
         )
         self.Add(self.wxgui_waterfallsink2_0.win)
-        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(1, (firdes.low_pass(1.0, samp_rate, 7.5e4, 1e4)), center_freq, samp_rate)
+        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(1, (firdes.low_pass(1.0, samp_rate, 1.5e5, 1e4)), center_freq, samp_rate)
+        self.fir_filter_xxx_0 = filter.fir_filter_fff(samp_rate/audio_rate, (firdes.low_pass(1.0, samp_rate, audio_rate/2-1000, 1000)))
+        self.fir_filter_xxx_0.declare_sample_delay(0)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.blocks_sub_xx_0 = blocks.sub_cc(1)
+        self.blocks_multiply_xx_1 = blocks.multiply_vff(1)
+        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
         self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, "/home/mero/kafu/grc_demo_2021/konzerv.iq", True)
+        self.blocks_divide_xx_0 = blocks.divide_ff(1)
+        self.blocks_delay_0 = blocks.delay(gr.sizeof_gr_complex*1, 1)
+        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(1)
+        self.blocks_complex_to_float_1 = blocks.complex_to_float(1)
+        self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
+        self.blocks_add_xx_0 = blocks.add_vff(1)
         self.audio_sink_0 = audio.sink(audio_rate, "", True)
-        self.analog_wfm_rcv_0 = analog.wfm_rcv(
-        	quad_rate=samp_rate,
-        	audio_decimation=samp_rate/audio_rate,
-        )
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_wfm_rcv_0, 0), (self.audio_sink_0, 0))    
+        self.connect((self.blocks_add_xx_0, 0), (self.blocks_divide_xx_0, 0))    
+        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_multiply_xx_0, 0))    
+        self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_multiply_xx_1, 0))    
+        self.connect((self.blocks_complex_to_float_1, 0), (self.blocks_multiply_xx_0, 1))    
+        self.connect((self.blocks_complex_to_float_1, 1), (self.blocks_multiply_xx_1, 1))    
+        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_divide_xx_0, 1))    
+        self.connect((self.blocks_delay_0, 0), (self.blocks_sub_xx_0, 1))    
+        self.connect((self.blocks_divide_xx_0, 0), (self.fir_filter_xxx_0, 0))    
+        self.connect((self.blocks_divide_xx_0, 0), (self.wxgui_waterfallsink2_1, 0))    
         self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))    
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_add_xx_0, 0))    
+        self.connect((self.blocks_multiply_xx_1, 0), (self.blocks_add_xx_0, 1))    
+        self.connect((self.blocks_sub_xx_0, 0), (self.blocks_complex_to_float_1, 0))    
         self.connect((self.blocks_throttle_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))    
         self.connect((self.blocks_throttle_0, 0), (self.wxgui_waterfallsink2_0, 0))    
-        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.analog_wfm_rcv_0, 0))    
-        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.wxgui_waterfallsink2_1, 0))    
+        self.connect((self.fir_filter_xxx_0, 0), (self.audio_sink_0, 0))    
+        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.blocks_complex_to_float_0, 0))    
+        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.blocks_complex_to_mag_squared_0, 0))    
+        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.blocks_delay_0, 0))    
+        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.blocks_sub_xx_0, 0))    
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -126,9 +146,10 @@ class konzerv_rx(grc_wxgui.top_block_gui):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
-        self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
+        self.fir_filter_xxx_0.set_taps((firdes.low_pass(1.0, self.samp_rate, self.audio_rate/2-1000, 1000)))
         self.wxgui_waterfallsink2_1.set_sample_rate(self.samp_rate)
-        self.freq_xlating_fir_filter_xxx_0.set_taps((firdes.low_pass(1.0, self.samp_rate, 7.5e4, 1e4)))
+        self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
+        self.freq_xlating_fir_filter_xxx_0.set_taps((firdes.low_pass(1.0, self.samp_rate, 1.5e5, 1e4)))
 
     def get_center_freq(self):
         return self.center_freq
@@ -144,9 +165,10 @@ class konzerv_rx(grc_wxgui.top_block_gui):
 
     def set_audio_rate(self, audio_rate):
         self.audio_rate = audio_rate
+        self.fir_filter_xxx_0.set_taps((firdes.low_pass(1.0, self.samp_rate, self.audio_rate/2-1000, 1000)))
 
 
-def main(top_block_cls=konzerv_rx, options=None):
+def main(top_block_cls=konzerv_rx_atan, options=None):
 
     tb = top_block_cls()
     tb.Start(True)
